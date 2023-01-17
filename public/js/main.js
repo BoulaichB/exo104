60b68fb494bc0dde075cb16d2b7d7b3746b786e5
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-17 09:57:51 +0100

    Ajout/suppression de la classe text-style en fonction de si elle est présente ou non

--- public/js/main.js
+++ public/js/main.js
@@ -12,5 +12,12 @@ document.querySelector('h3').addEventListener('dblclick', ()=>{
     document.querySelector('h3').classList.add('text-error');
 });
 // ### 3. Trouve une methode qui rajoute la class text-style sur le paragraphe mais qui en meme temps vérifie si la class est déjà dessus, dans ce cas la cette methode est capable de la retirer 
+document.querySelector('p').addEventListener('click', ()=>{
+    if(document.querySelector('p').classList.contains('text-style')){
+        document.querySelector('p').classList.remove('text-style');
+    } else{
+        document.querySelector('p').classList.add('text-style');
+    }
+})
 // ### 4. Dans l'avant dernier paragraphe, tu as trois span, créer un programme qui met la class bolder sur le mot sur le quel on clique 
 // ### 5. Dans le dernier paragraphe tu as trois span,, créer un programme qui rajoute la class "bolrder-red" sur le mot sur le quel on clique mais retire cette class au deux autres, si elles l'ont !
